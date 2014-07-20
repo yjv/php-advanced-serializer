3fdaa7321493680c67f4cfd0923571b240869d4d
Yosef Deray <yosefderay770@gmail.com>
2014-07-20 01:33:38 -0400

    added the globals for holding the normalizers

--- php_advanced_serializer.h
+++ php_advanced_serializer.h
@@ -8,7 +8,16 @@ PHP_MINIT_FUNCTION(advanced_serializer);
 PHP_FUNCTION(advanced_serialize);
 PHP_FUNCTION(set_serialize_normalizer);
 
+typedef struct _advanced_serializer_globals {
+} advanced_serializer_globals;
+
 extern zend_module_entry advanced_serializer_module_entry;
-#define phpext_hello_ptr &advanced_serializer_module_entry
+#define phpext_advanced_serializer_ptr &advanced_serializer_module_entry
+
+#ifdef ZTS
+#define ASERIALIZER(v) TSRMG(advanced_serializer_globals_id, advanced_serializer_globals *, v)
+#else
+#define ASERIALIZER(v) (advanced_serializer_globals.v)
+#endif
 
 #endif
